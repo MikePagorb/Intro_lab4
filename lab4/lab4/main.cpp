2b5c4f009a03293b93f3a5d8efc9b0ed326dc6b0
Mikhailo Kholmohorov <mykhailo1257@gmail.com>
2019-06-19 11:57:42 +0300

    create final lap

--- lab4/lab4/main.cpp
+++ lab4/lab4/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <string>
+#include "IMG.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
+	char* inputFile = argv[1];
+	char* outputFile = argv[2];
+	double coefficient = stod(argv[3]);
 
+	cout << " Increase the image " << coefficient << " times..." << endl;
+	image picture, resizePicture;
+	readImage reader;
+	resizeImage resizer;
+	saveImage saver;
+	picture = reader.read(inputFile);
+	resizePicture = resizer.resize(picture, coefficient);
+	saver.save(resizePicture, outputFile);
+	cout << " Increase done successfully!" << endl;
+	cout << " Image save to " << outputFile;
 	system("pause");
 	return 0;
 }
